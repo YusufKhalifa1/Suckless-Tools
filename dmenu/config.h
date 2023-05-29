e05a3eb7134ce1b8af0795d9f8f20687b62bca69
YusufKhalifa1 <yusufkhalifa5151@gmail.com>
2023-05-29 19:42:03 +0300

    test

--- dmenu/config.h
+++ dmenu/config.h
@@ -10,12 +10,12 @@ static const char *fonts[] = {
 	"MesloLGS Nerd Font:size=9"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-/*static const char *colors[SchemeLast][2] = {
+static const char *colors[SchemeLast][2] = {
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
-};*/
-#include "/home/yusuf/.cache/wal/colors-wal-dmenu.h"
+};
+//#include "/home/yusuf/.cache/wal/colors-wal-dmenu.h"
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int columns    = 5;
 static unsigned int lines      = 5;
