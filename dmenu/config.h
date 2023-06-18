0559d78bd7c72e04ff6dd1a33d96b16dea5b2525
YusufKhalifa1 <yusufkhalifa5151@gmail.com>
2023-06-18 17:11:19 +0300

    Added Dwmblocks and edited st and dmenu; (decreased the number of patches)

--- dmenu/config.h
+++ dmenu/config.h
@@ -1,24 +1,21 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
-static int centered = 1;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrains Mono:size=9"
+	"JetBrains Mono:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
+/*static const char *colors[SchemeLast][2] = {
+	     fg         bg       
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
-};
-//#include "/home/yusuf/.cache/wal/colors-wal-dmenu.h"
-/* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int columns    = 5;
-static unsigned int lines      = 5;
+};*/
+#include "/home/yusuf/.cache/wal/colors-wal-dmenu.h"
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
 
 /*
  * Characters not considered part of a word while deleting words
